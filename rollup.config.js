f7d2339d6bd81e0ca5cef94d8a292339fffd51fc
Sebastian Oscar Lopez <sebastianoscarlopez@gmail.com>
2023-09-02 20:25:29 -0300

    feat: mesh2012 bs as

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,24 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import css from "rollup-plugin-import-css";
 import copy from 'rollup-plugin-copy'
-
+import incremental from '@mprt/rollup-plugin-incremental'
 // import postcss from 'rollup-plugin-postcss'
 
 // rollup.config.mjs
 export default {
   input: 'src/main.js',
+  treeshake: false,
   output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
+    dir: 'dist',
+    format: 'cjs',
+    // // //ATTENTION: preserveModules must be enabled!
+    // preserveModules: true,
+    // preserveModulesRoot: 'src',
+    //ATTENTION: minifyInternalExports must be disabled!
+    // minifyInternalExports: false,
   },
   plugins: [
+    // incremental(),
     commonjs({ sourceMap: false }),
     nodeResolve(),
     css({
@@ -31,6 +38,9 @@ export default {
           dest: 'dist'
         }
       ]
-    })
+    }),
+    //ATTENTION: this fixes issues with syntheticNamedExports in commonjs modules
+    //it should be last 
+    // incremental.fixSNE(),
   ]
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,32 @@
-import { Cartesian3, createOsmBuildingsAsync, Ion, Math, Terrain, Viewer } from 'cesium';
+import { Cartesian3, Ion, Cesium3DTileset, Math, Terrain, Viewer } from 'cesium';
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
 window.onload = () => {
-  Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyNjI1ZGZkYy01ZmE1LTRkOWYtYmYwZi1jOGFlYmUxZjI1YjgiLCJpZCI6MTYzNjQyLCJpYXQiOjE2OTM0NDczNzd9.wfJDC4VVEonMFKJzVpajKXZ0jTtFOUt1DVnqGgqwSOw';
+  Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjYTUzNjU0Ny0xMTgyLTQ0NWItYWJhOC1hMWYzMDc2NTMyMTciLCJpZCI6MTYzNjQyLCJpYXQiOjE2OTM3MDE5MTd9.SWmsi_pUOyNOLYU6UZl3biE9HWhO0ioOAQ8qFU7uCIs';
 
   const viewer = new Viewer('cesiumContainer', {
-    terrain: Terrain.fromWorldTerrain(),
+    // terrain: Terrain.fromWorldTerrain(),
+    // globe: false,
+  });
+
+  console.log("viewer", viewer);
+  Cesium3DTileset.fromUrl(
+    "https://storage.googleapis.com/mesh3d/mesh2021/tileset.json"
+    // "http://localhost:8002/tileset.json"
+  )
+  .then(tileset => {
+    viewer.scene.primitives.add(tileset);
+    viewer.camera.flyTo({
+      destination: Cartesian3.fromDegrees(-58.4371312, -34.6134264, 320),
+      // destination: Cartesian3.fromDegrees(-74.0444987716782, 40.689434025350025, 320),
+      orientation: {
+        heading: Math.toRadians(0.0),
+        pitch: Math.toRadians(0.0),
+      }
+    });  
+  })
+  .catch(error => {
+    console.log(error);
   });
 
   // Fly the camera to San Francisco at the given longitude, latitude, and height.
